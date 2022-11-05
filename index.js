98ad5b7356373ddb5e10438e1814d84b71a2b9d9
Trilvid <trillztech34@gmail.com>
2022-11-05 02:20:30 +0100

    edited index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
- const app = require('./app');
+//  const app = require('./app');
  
- const port = 3000;
- app.listen(port, () => {
-     console.log(`App is running on port ${port}....`);
- }); 
+//  const port = 3000;
+//  app.listen(port, () => {
+//      console.log(`App is running on port ${port}....`);
+//  }); 
  
+exports.data = {
+    "slackUsername": "Trilvid",
+       "backend": true,
+       "age": 20,
+       "bio": "Hello, i am Divine Obinna and i am currently a backend intern with Zuri internship HNG9"   
+   }
